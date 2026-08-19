85b02aaaa2ea6ff98a7bd5739d1f53c8754458b0
Anish Bandapelli <anish.bandapelli@gmail.com>
2026-08-18 20:22:08 -0400

    Refactor context a/b into Thread and fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,38 @@ struct Thread {
   bool status;
 };
 
+Thread *g_t1, *g_t2;
+
 void funcA() {
   std::cout << "A: pre swap\n";
-  // swapcontext(&context_a, &context_b);
-  // structtd::cout << "A: post swap\n";
+  swapcontext(&g_t1->context, &g_t2->context);
+  std::cout << "A: post swap\n";
 }
 
-/*
-voidid funcB() {
+void funcB() {
   std::cout << "B: start\n";
-  swapcontext(&context_b, &context_a);
+  swapcontext(&g_t2->context, &g_t1->context);
 }
-*/
 
 int main() {
   std::cout << "Before Context Switch\n";
   Thread t1;
+  g_t1 = &t1;
   t1.stack = new char[64 * 1024];
   getcontext(&t1.context);
   t1.context.uc_stack.ss_sp = t1.stack;
-  t1.context.uc_stack.ss_size = sizeof(t1.stack);
+  t1.context.uc_stack.ss_size = 64 * 1024;
   t1.context.uc_link = &context_main;
   makecontext(&t1.context, funcA, 0);
+
+  Thread t2;
+  g_t2 = &t2;
+  t2.stack = new char[64 * 1024];
+  getcontext(&t2.context);
+  t2.context.uc_stack.ss_sp = t2.stack;
+  t2.context.uc_stack.ss_size = 64 * 1024;
+  t2.context.uc_link = &t1.context;
+  makecontext(&t2.context, funcB, 0);
 
   swapcontext(&context_main, &t1.context);
   std::cout << "After Context Switch\n";
